a155bb2b649fc3e99907d7dfe4d42638e9552d6d
Classroom <ryancruiz97@gmail.com>
2024-05-24 11:44:10 +0800

    tweaked and added flow chart, comments added

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Created using cpp with three chapters
 - score
 - login (static)
 - displaying score per chapter
+
+# FLOW CHART
+![examination_flow_chart-Page-1 drawio](https://github.com/Ryan-Cruiz/examination-cpp/assets/109123846/c842f957-b6fe-4176-a7af-2b11a0b1b23e)
+![examination_flow_chart-Page-2 drawio](https://github.com/Ryan-Cruiz/examination-cpp/assets/109123846/7c260006-7e17-4579-8289-9a2400af05b6)
+![examination_flow_chart-Page-3 drawio](https://github.com/Ryan-Cruiz/examination-cpp/assets/109123846/dc423d43-d0ea-4146-8bef-3f04a34e3b66)
--- main.cpp
+++ main.cpp
@@ -3,33 +3,37 @@
 #include <cstring>
 #include <cctype> // for using toupper
 using namespace std;
-void chapters(int &choice,string &file,string &reviewer,int &chapter){
+// THIS WILL PRINT SPECIFIC CHAPTER
+void chapters(int &choice,string &file,string &reviewer,int &chapter,string &chapterFlavorText){
     if(choice == 1){
+    	chapterFlavorText = "Chapter 1 - Fundamentals of C++\n";
         cout << "Chapter 1 - Fundamentals of C++\n";
         file = "chapter1.txt";
         reviewer = "reviewer1.txt";
     }else if(choice == 2){
+    	chapterFlavorText = "Chapter 2 - Looping and Array\n";
         cout << "Chapter 2 - Looping and Array\n";
         file = "chapter2.txt";
         reviewer = "reviewer2.txt";
     }else if(choice == 3){
-        cout << "Chapter 3 - References and Pointers\n";
-        file = "chapter3.txt";
-        reviewer = "reviewer3.txt";
+    	chapterFlavorText = "Chapter 3 - References and Pointers\n"; // TAKE CHAPTER FLAVOR TEXT
+        cout << "Chapter 3 - References and Pointers\n"; // PRINT CHAPTER NAME
+        file = "chapter3.txt"; // TAKE CHAPTER TXT NAME
+        reviewer = "reviewer3.txt"; // TAKE REVIEWER TXT NAME
     }
     chapter = choice;
     do{
         cout << "1. Review\n";
         cout << "2. Take Quiz\n";
+        cout << "3. Back\n";
         cout << "Input: ";
         cin >> choice;
-        if(choice == 1){
-            break;
-        }else if(choice == 2){
+        if(choice == 1 || choice == 2 || choice == 3){
             break;
         }
     }while(true);
 }
+// THIS WILL PRINT CHAPTERS
 void printChapter(int &choice){
    cout << "WELCOME TO EXAMINATION!" <<endl;
         cout << "Choose what Chapter | to exit input -1" << endl;
@@ -49,7 +53,10 @@ int main(){
 	int choice, chapter;
     int chapter_scores[3] = {0,0,0};
     string file,reviewer;
-	cout << "Examination Program" << endl;
+    string chapterFlavorText;
+//	cout << "Examination Program" << endl;
+	cout << "LOG IN" << endl;
+//	LOGIN LOOP
 	do{
 		cout << "Username: ";
 		cin >> username;
@@ -62,6 +69,7 @@ int main(){
 			cout << "Failed! Please Try Again." << endl;
 		}
 	}while(true);
+//	MAIN MENU LOOP 
 	do{
         printChapter(choice);
         if(choice <= 4 && choice > 0){
@@ -74,33 +82,44 @@ int main(){
                 system("pause");
                 system("CLS");
             }else{
-                chapters(choice,file,reviewer,chapter);
+                chapters(choice,file,reviewer,chapter,chapterFlavorText);
             }
-            if(choice == 1){
-                ifstream MyReadFile(reviewer);
+            // THIS WILL DO READ THE FILE REVIEWER PRIOR TO WHAT CHAPTER PICKED
+            while(choice == 1){
+                ifstream MyReadFile(reviewer); // WILL READ THE FILE REVIEWER .TXT
                 int i = 0;
                 cout << "\n--------------------------------------\n";
                  while(getline(MyReadFile, questions)){
                     cout << questions << endl;
                 }
                 cout << "\n--------------------------------------\n";
-                cout << "2. Take Quiz\n";
+                cout << chapterFlavorText; // PRINT CHAPTER FLAVOR TEXT
+                cout << "1. Review\n";
+                cout << "2. Take Quiz\n"; // TAKE QUIZ
+                cout << "3. Back\n";
                 cout << "Input: ";
                 cin >> choice;
+                if(choice == 2 || choice == 3){
+                	system("CLS");
+					break;
+				}
                 system("CLS");
             }
+            // THIS WILL DO READ THE FILE QUESTION PRIOR TO WHAT USER PICKED
             if(choice == 2){
-                ifstream MyReadFile(file);
+                ifstream MyReadFile(file); // WILL READ THE FILE CHAPTER .TXT
                 int score = 0;
                 int index = 0;
                 while(getline(MyReadFile, questions)){
                     cout << questions << endl;
+                    // CHECK IF THE LINE IS * THEN PROCEED TO INPUT ANSWER
                     if(questions == "*"){
                         cout << "Your answer: ";
                         cin >> your_answer[index];
                         index++;
                     }
                 }
+                // LOOP THE USER'S ANSWERS
                 for(int i=0;i<key_answer[chapter-1].length();i++){
                     upper = toupper(your_answer[i]);
                     cout << upper << ',';
@@ -108,12 +127,16 @@ int main(){
                         score += 1;
                     }
                 }
+                // STORE SCORE IN CHAPTER_SCORES
                 chapter_scores[chapter-1] = score;
                 cout << "\nScore: " << chapter_scores[chapter-1] << endl;
                 cout << "\n\n";
                 system("pause");
                 system("CLS");
             }
+            if(choice == 3){
+            	system("CLS");
+			}
         }else if(choice == -1){
             break;
         }
